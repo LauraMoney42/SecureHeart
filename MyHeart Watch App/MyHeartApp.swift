e1acb64f18ad3f3b549ef371b06d1eed9d49a9bf
Laura Money <LauraC.Money@gmail.com>
2025-08-25 19:56:24 -0700

    Initial Commit

---
+++ MyHeart Watch App/MyHeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHeartApp.swift
+//  MyHeart Watch App
+//
+//  Created by Laura Money on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MyHeart_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyHeart/MyHeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHeartApp.swift
+//  MyHeart
+//
+//  Created by Laura Money on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MyHeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
